45eb59126f64b0ce86239ba2eab5f927b4a384b7
ajay-kumar-youngsoft <ajay.kumar@gmail.com>
2024-03-22 12:34:15 +0530

    initialization of project setup

---
+++ src/main/java/emenu/EMenuApplication.java
@@ -0,0 +1,13 @@
+package emenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EMenuApplication.class, args);
+	}
+
+}
